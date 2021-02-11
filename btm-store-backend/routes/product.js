601cd3110bb74209ae1035ac3dd2819e69254a5f
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-02-11 14:41:12 +0530

    testing product part backend side

---
+++ btm-store-backend/routes/product.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} = require("../controllers/product");
+const { protect, admin } = require("../middleware/authMiddleware");
+const router = express.Router();
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
+
+module.exports = router;
